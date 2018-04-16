441c2cb3aafd86db1575d9a3cf6f040c474ff113
Jatin CR <TheJCR@Jatins-MacBook-Air.local>
2018-04-16 15:22:59 -0400

    Updated ExprPretty - Though I won't Be Using It

--- Assign3/ExprPretty.hs
+++ Assign3/ExprPretty.hs
@@ -6,7 +6,11 @@ parens :: String -> String
 parens ss = "(" ++ ss ++ ")"
 
 instance Show a => Show (Expr a) where
-  show (Mult e1 e2) = parens (show e1) ++ " !* " ++ parens (show e2)
   show (Add e1 e2)  = parens (show e1) ++ " !+ " ++ parens (show e2)
+  show (Mult e1 e2) = parens (show e1) ++ " !* " ++ parens (show e2)
+  show (Sin e)      = " Sin " ++ parens (show e)
+  show (Cos e)      = " Cos " ++ parens (show e)
+  show (Log a e)    = " Log Base " ++ (show a) ++ " Of " ++ parens (show e)
+  show (Exp e)      = " (Natural) e ^ " ++ parens (show e)
   show (Var ss)     = parens $ "var \"" ++ ss ++ "\""
   show (Const x)    = parens $ "val " ++ show x
